f6cc23573243babe5eae7eed14f368f0f115bbc3
Matrix <mubaraqsanusi908@gmail.com>
2017-01-31 13:31:13 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sanwo</title>
+  
+  <link rel="stylesheet" href="css/reset.min.css">
+
+      <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
+            <link rel="stylesheet" href="css/style.css">
+
+      <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+
+      
+</head>
+
+<body>
+
+<div class="navbar navbar-fixed-top  navbar-default">
+      <div class="navbar-inner">
+          <a class="brand" href="/">SANWO</a>
+      </div>
+    </div>
